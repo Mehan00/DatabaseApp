14c441acfe03b1af5978fc07760d2b0d5de3617b
Jakub <73178715+Mehan00@users.noreply.github.com>
2021-06-15 14:18:04 +0200

    Add files via upload

--- src/DataBasePanel.java
+++ src/DataBasePanel.java
@@ -2,20 +2,32 @@ import java.awt.Dimension;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 //	#Klasa ktora obsluguje panel sterowania i komunikuje sie z
 class DataBasePanel extends JPanel{
 	
     private AppClass parentFrame;
 	private JLabel lab1;
+    private JTextArea textArea;
 
 	
 	public DataBasePanel(AppClass parentFrame) {
 		try{
             setPreferredSize(new Dimension(500,300));
 
-            lab1 = new JLabel("DatabasePanel");
-            add(lab1);
+            
+            textArea = new JTextArea ();
+            textArea.setPreferredSize(new Dimension(400,280));   
+            textArea.setLineWrap(true);
+            textArea.setEditable(false);
+            
+
+            JScrollPane scroll = new JScrollPane (textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+            add(scroll);
+            
+
         }
         catch(Exception err){
         	new UnknownException("Exception caught in class DatabasePanel. In the constructor.\n", err);
